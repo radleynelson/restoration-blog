3ed01719122da3a2486474507820c3b4f30104bf
radleynelson <nelson.radley@gmail.com>
2018-04-16 21:06:07 -0600

    Done with this blog

---
+++ src/components/BlogFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <transition appear name="v--mask">
+    <footer v-once class="blog__footer">
+        <span>Designed by Radley Nelson</span>
+    </footer>
+  </transition>
+</template>
+
+<script>
+export default { name: 'blog-footer' }
+</script>
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,78 @@
+<template>
+  <transition name="post">
+    <article v-if="post" class="post">
+      <header class="post__header">
+        <h2 class="post__title">{{ title }}</h2>
+
+        <h3 class="post__meta">by <router-link class="post__author"
+          :to="`/by/${kebabify(author)}`">{{ author }}</router-link>
+          <span class="post__sep"></span>
+          <time>{{ prettyDate(published) }}</time>
+        </h3>
+
+        <blockquote class="post__subtitle">{{ description }}</blockquote>
+      </header>
+
+      <section class="post__body rte" v-html="content"></section>
+
+    </article>
+  </transition>
+</template>
+
+<script>
+import VueDisqus from 'vue-disqus/VueDisqus'
+import { kebabify, prettyDate } from '../helpers'
+
+export default {
+  name: 'blog-post',
+  resource: 'BlogPost',
+  components: { VueDisqus },
+  props: { post: String },
+
+  data() {
+    return {
+      title: '',
+      author: '',
+      content: '',
+      published: '',
+      description: '',
+      commentsReady: false
+    }
+  },
+
+  watch: {
+    post(to, from) {
+      if (to === from || !this.post) return;
+
+      this.commentsReady = false
+      this.$getResource('post', to)
+        .then(this.showComments)
+    }
+  },
+
+  methods: {
+    kebabify,
+    prettyDate,
+    showComments() {
+      setTimeout(() => {
+        this.commentsReady = true
+      }, 1000)
+    }
+  },
+
+  beforeMount() {
+    if (!this.post) return;
+    this.$getResource('post', this.post)
+      .then(this.showComments)
+  }
+}
+</script>
+
+<style media="screen">
+  h2{
+    font-size: 35px;
+    font-weight: bold;
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+</style>
